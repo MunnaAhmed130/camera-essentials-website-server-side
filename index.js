76719db407ecd8982ed16231113fe848d22d70a2
Munna Ahmed <munnaahmed2025@gmail.com>
2023-03-02 22:16:02 +0600

    commit

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ const client = new MongoClient(uri, {
 app.use(cors());
 app.use(express.json());
 
-await client.connect();
-
 async function run() {
     try {
+        client.connect();
+
         const database = client.db("camera_essentials");
         const productsCollection = database.collection("products");
         const usersCollection = database.collection("users");
