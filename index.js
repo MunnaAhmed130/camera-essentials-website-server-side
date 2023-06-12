0db04670d34bd9a1c2ecfeaaa41cc57cdfcd0ad3
Munna Ahmed <munnaahmed2025@gmail.com>
2023-06-12 20:03:24 +0600

    commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 const port = process.env.PORT || 4000;
 
 // uri
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dgg2e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dgg2e.mongodb.net/?retryWrites=true&w=majority"`;
 // console.log(uri);
 
 // client
